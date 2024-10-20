d68ac79f140e352e79c163a005f01ab26c442f0c
Ashneet Jha <141767681+ashneetjha@users.noreply.github.com>
2024-10-20 22:10:35 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// GSAP Animations
+gsap.from(".title", { duration: 1, y: -50, opacity: 0 });
+gsap.from(".subtitle", { duration: 1, y: 50, opacity: 0, delay: 0.5 });
+gsap.from(".btn-primary", { duration: 1, scale: 0, opacity: 0, delay: 1 });
